91638b1597012527a11bd6676ca3d6d2e87ec0df
jangidritesh570-cmyk <jangidritesh570@gmail.com>
2026-06-27 16:27:51 +0530

    emailfix

--- services/emailService.js
+++ services/emailService.js
@@ -3,15 +3,14 @@ import nodemailer from "nodemailer";
 export const sendEmail = async ({ email, subject, message }) => {  
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      service: process.env.SMTP_SERVICE,
-      secure: true,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
 
     const mailOptions = {
       from: process.env.SMTP_USER,
